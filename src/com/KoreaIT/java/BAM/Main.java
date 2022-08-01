88dd672cca785a3508a7c806f70418ebdd1e477d
Leekyujin <kyujinlee82@gmail.com>
2022-08-01 19:43:45 +0900

    게시물 상세 페이지 구현, 날짜X

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -1,14 +1,15 @@
 package com.KoreaIT.java.BAM;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
 		int artid = 0;
-		
 
 		List<Article> articles = new ArrayList<>();
 
@@ -43,18 +44,65 @@ public class Main {
 
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
-			} else {
-				System.out.println("존재하지 않는 명령어입니다.");
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				} else {
+					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("날짜 : %s\n", "2022-07-14 12:12:12");
+					System.out.printf("제목 : %s\n", foundArticle.title);
+					System.out.printf("내용 : %s\n", foundArticle.body);
+				}
+
+			} else if (cmd.startsWith("article delete ")) {
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						articles.remove(id - 1);
+						System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
+						break;
+					}
+
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				} else {
+					System.out.println("존재하지 않는 명령어입니다.");
+				}
 			}
+			System.out.println("==프로그램 끝==");
+			sc.close();
 		}
-		System.out.println("==프로그램 끝==");
-		sc.close();
 	}
 }
 
