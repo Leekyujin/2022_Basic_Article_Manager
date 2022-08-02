f932262c4cf9cd936dc4de8a0e7f102419313ac5
Leekyujin <kyujinlee82@gmail.com>
2022-08-02 18:55:27 +0900

    article list에 검색 기능 추가

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -34,14 +34,36 @@ public class App {
 				break;
 			}
 
-			if (cmd.equals("article list")) {
+			if (cmd.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다.");
 					continue;
 				}
+				
+				String searchKeyword = cmd.substring("article list".length()).trim();
+
+				System.out.printf("검색어 : %s\n", searchKeyword);
+
+				List<Article> forPrintArticles = articles;
+
+				if (searchKeyword.length() > 0) {
+					forPrintArticles = new ArrayList<>();
+
+					for (Article article : articles) {
+						if(article.title.contains(searchKeyword)) {
+							forPrintArticles.add(article);
+						}
+					}
+
+					if (forPrintArticles.size() == 0) {
+						System.out.println("검색 결과가 없습니다.");
+						continue;
+					}
+				}
+				
 				System.out.printf("번호  |     제목     |        %5s          |  조회\n", "날짜");
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
+					Article article = forPrintArticles.get(i);
 
 					System.out.printf("%3d   |    %4s      |   %5s   | %5d\n", article.id, article.title, article.regDate,
 							article.hit);
